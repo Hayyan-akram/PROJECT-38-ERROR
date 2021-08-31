abc7388310fc06f144d4f4b5147247ae41e19498
HAYYAN-AKRAM <74773145+Hayyan-akram@users.noreply.github.com>
2021-08-31 18:19:05 +0530

    Add files via upload

---
+++ PROJECT-38/trex-master/sketch.js
@@ -0,0 +1,202 @@
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
+
+var driver, player_moving, player_crashing;
+var ground, invisibleGround, groundImage;
+
+var cloudsGroup, cloudImage;
+var obstaclesGroup,obstacle1,obstacle2,obstacle3,obstacle4,obstacle5,obstacle6;
+
+var score=0;
+
+var gameOver, restart;
+
+
+function preload(){
+  player_moving =   loadImage("Driver.png");
+  player_crashing = loadImage("DriverCrash.png");
+  
+  groundImage = loadImage("track.png")
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle1.png");
+  obstacle3 = loadImage("obstacle1.png");
+  obstacle4 = loadImage("obstacle1.png");
+  obstacle5 = loadImage("obstacle1.png");
+  obstacle6 = loadImage("obstacle1.png");
+  
+  
+  gameOverImg = loadImage("gameOver.png");
+  restartImg = loadImage("restart.png");
+  
+  jumpSound = loadSound("CarMove.mp3");
+  dieSound = loadSound("CarCrash.mp3");
+  checkPointSound = loadSound("checkPoint.mp3"); 
+}
+
+function setup() {
+  createCanvas(1200, 1200);
+  
+  driver = createSprite(200,250,10,25);
+  driver.addImage("driving", player_moving);
+  driver.addImage("crashed", player_crashing);
+  driver.scale = 0.5;
+  
+  ground = createSprite(400,400,1200,1200);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -(6 + 3*score/1000);
+  
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  invisibleGround = createSprite(400,400,400,10);
+  invisibleGround.visible = false;
+  
+  cloudsGroup = new Group();
+  obstaclesGroup = new Group();
+  
+  score = 0;
+}
+
+function draw() {
+  background(255);
+  text("Score: "+ score, 500,50);
+  
+  if (gameState===PLAY){
+    score = score + Math.round(getFrameRate()/60);
+    
+  
+    if(keyDown("space") && driver.y >= 159) {
+      jumpSound.play();
+      driver.velocityY = -14;
+    }
+  
+    driver.velocityY = driver.velocityY + 0.8
+  
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  
+    driver.collide(invisibleGround);
+    spawnClouds();
+    spawnObstacles();
+    
+    if (score>0 && score%100 === 0){
+      checkPointSound.play();
+    }
+  
+    if(obstaclesGroup.isTouching(driver)){
+      dieSound.play();  
+      gameState = END;
+        
+    }
+  }
+  else if (gameState === END) {
+    gameOver.visible = true;
+    restart.visible = true;
+    
+    //set velcity of each game object to 0
+    driver.velocityX = 0;
+    driver.velocityY = 0;
+    obstaclesGroup.setVelocityXEach(0);
+    cloudsGroup.setVelocityXEach(0);
+    
+    //change the trex animation
+    driver.changeImage("crashed",player_crashing);
+    
+    //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+    
+    if(mousePressedOver(restart)) {
+      reset();
+    }
+  }
+  
+  
+  drawSprites();
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 200;
+    
+    //adjust the depth
+    cloudImage.depth = driver.depth;
+    driver.depth = driver.depth + 1;
+    
+    //add each cloud to the group
+    cloudsGroup.add(cloud);
+  }
+  
+}
+
+function spawnObstacles() {
+  if(frameCount % 60 === 0) {
+    var obstacle = createSprite(600,165,10,40);
+    obstacle.velocityX = -(6 + 3*score/100);
+    
+    //generate random obstacles
+    var rand = Math.round(random(1,1));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+    
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle1);
+    obstaclesGroup.add(obstacle2);
+    obstaclesGroup.add(obstacle3);
+    obstaclesGroup.add(obstacle4);
+    obstaclesGroup.add(obstacle5);
+    obstaclesGroup.add(obstacle6);
+  }
+}
+
+function reset(){
+  gameState = PLAY;
+  ground.velocityX = -(6 + 3*score/100);
+  gameOver.visible = false;
+  restart.visible = false;
+  
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  
+  driver.changeAnimation("driving",player_moving);
+  
+  score = 0;
+  
+}
